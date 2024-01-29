9a8b5e946fba09b63138a29ba69fecd68b4d02e5
Ravi Kumar <ravi14577@gmail.com>
2024-01-29 10:48:02 +0530

    all the grids complete

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,27 +2,30 @@ import React from "react";
 import Home_Table from "./Home_Table";
 import { MdArrowOutward } from "react-icons/md";
 import { FaArrowRight } from "react-icons/fa";
+import SemiCircleProgressBar from "react-progressbar-semicircle";
 
 const Dashboard = () => {
-
   return (
     <>
       <div className="m-10 ">
         <div class="grid grid-cols-2 md:grid-cols-3 gap-10 pt-2 justify-between">
           {/* 1st box */}
-          <div class="py-2 bg-white drop-shadow-xl p-4 border rounded-xl">
-            <h2 class="font-semibold text-xl pt-4">Revenue</h2>
+          <div class="h-[250px] py-2 bg-white drop-shadow-xl p-4 border rounded-xl">
+            <h2 class="font-semibold text-xl py-4">Revenue</h2>
             <div class="flex flex-row sm:flex-row overflow-hidden">
               <p class="flex items-center text-black text-3xl font-bold sm:mr-20 mb-4 sm:mb-0">
                 15% <MdArrowOutward className="text-xl text-green-500 ml-1" />
               </p>
             </div>
 
-            <div class="flex flex-col text-center sm:text-right">
-              <p class="text-gray-300">Increase compared to last week</p>
+            <div class="flex flex-col">
+              <p>Increase compared to last week</p>
             </div>
 
-            <a href="#" class="text-[#A94438] mt-2 flex items-center text-base">
+            <a
+              href="#"
+              class="text-[#A94438] mt-20 flex items-center text-base"
+            >
               Revenues report <FaArrowRight />
             </a>
           </div>
@@ -31,41 +34,50 @@ const Dashboard = () => {
 
           {/* 2nd box start */}
 
-          <div class="py-2 bg-white drop-shadow-xl p-4 border rounded-xl">
-            <h2 class="font-semibold text-xl pt-4">Lost deals</h2>
+          <div class="h-[250px] py-2 bg-white drop-shadow-xl p-4 border rounded-xl">
+            <h2 class="font-semibold text-xl py-4">Lost deals</h2>
             <div class="flex flex-row sm:flex-row overflow-hidden">
               <p class="flex items-center text-black text-3xl font-bold sm:mr-20 mb-4 sm:mb-0">
                 4%
               </p>
             </div>
 
-            <div class="flex flex-col text-center sm:text-right">
-              <p class="text-gray-300">You closed 96 out of 100 deals</p>
+            <div class="flex flex-col">
+              <p>You closed 96 out of 100 deals</p>
             </div>
 
-            <a href="#" class="text-[#A94438] mt-2 flex items-center text-base">
-              All deals <FaArrowRight />
+            <a
+              href="#"
+              class="text-[#A94438] mt-20 flex items-center text-base"
+            >
+              All deals
+              <FaArrowRight />
             </a>
           </div>
+
           {/* <!-- 2nd box end --> */}
 
           {/* <!-- 3rd box start --> */}
-          <div class="py-2 bg-white drop-shadow-xl p-4 border rounded-xl">
-            <h2 class="font-semibold text-xl pt-4">Quater goal</h2>
-            <div class="flex flex-row sm:flex-row overflow-hidden">
-              {/* add gere */}
-         
-              {/* done here/ */}
-            </div>
 
-            <div class="flex flex-col text-center sm:text-right">
-              <p class="text-gray-300">Increase compared to last week</p>
+          <div className="h-[250px] py-2 bg-white drop-shadow-xl p-4 border rounded-xl">
+            <h2 className="font-semibold text-xl py-4">Quarter goal</h2>
+            <div className="flex items-center justify-center font-bold text-3xl">
+              <SemiCircleProgressBar
+                percentage={84}
+                showPercentValue
+                className="text-white bg-orange-500 "
+              />
             </div>
+            <a
+  href="#"
+  className="text-[#A94438] mt-8 flex items-center justify-center text-base"
+>
+  All goals
+  <FaArrowRight className="ml-2" />
+</a>
 
-            <a href="#" class="text-[#A94438] mt-2 flex items-center text-base">
-              All goals <FaArrowRight />
-            </a>
           </div>
+
           {/* 3rd box end */}
         </div>
 
